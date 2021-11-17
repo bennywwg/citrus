13a14368b928d415f291b760ffd3ed2057a38ee0
Benny Wysong-Grass <bennywwg@gmail.com>
2021-11-16 19:11:24 -0600

    code quality

--- imgui-test/src/main.rs
+++ imgui-test/src/main.rs
@@ -31,18 +31,6 @@ impl Element for B {
         println!("B: val = {}", self.val);
         self.val += 10;
     }
-    /*
-    fn ecs_serialize(&self) -> serde_json::Value {
-        serde_json::to_value(&self).unwrap()
-    }
-    fn ecs_deserialize(&mut self, data: serde_json::Value) {
-        match serde_json::from_value::<Self>(data) {
-            Ok(parsed) => *self = parsed,
-            Err(er) => println!("{:?}", er)
-        };
-    }
-    */
-    
     fn fill_ui(&mut self, ui: &imgui::Ui, man: &mut Manager) {
         ecs::editor_helpers::select_entity(&mut self.other, "other", ui, man);
         ecs::editor_helpers::select_element(&mut self.ele, "ele", ui, man);
@@ -50,19 +38,18 @@ impl Element for B {
 }
 
 fn main() {
-    let mut manager = Manager::new();
+    let mut ma =        Manager::new();
+    let mut se =        SceneSerde::new();
+    let mut ed =        SceneEditor::new();
 
-    manager.create_entity("Baba booie".to_string());
-
-    let mut se = SceneSerde::new();
-    let mut ed = SceneEditor::new();
+    ma.create_entity("Baba booie".to_string());
 
     se.register_element_creator(A { val: 0 }, "PosRot");
     se.register_element_creator(B { val: 2, other: EntAddr::new(), ele: EleAddr::new() }, "Element B");
 
     let system = support::init(file!());
     system.main_loop(move |_, ui| {
-        ed.render(ui, &mut se, &mut manager);
-        manager.resolve();
+        ed.render(ui, &mut se, &mut ma);
+        ma.resolve();
     });
 }
